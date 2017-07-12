2f4d565b837d8d3cdcc6b0c624291c627e4209a0
Jenn Le <jenn.thakugan@icloud.com>
2017-07-11 22:46:16 -0500

    Testing travis ci with correct tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Testing Travis CI unit testing!';
+  title = 'app works!';
 }
