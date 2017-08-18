3517280c122fcb2dc4f964cd6284cf927780cd67
Jenn Le <jenn.thakugan@icloud.com>
2017-08-17 21:11:58 -0500

    Added footer stuff

---
+++ src/app/jenn-footer/jenn-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jenn-footer',
+  templateUrl: './jenn-footer.component.html',
+  styleUrls: ['./jenn-footer.component.scss']
+})
+export class JennFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
