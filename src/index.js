4df5624b1386560afc06dc122f3c72c3a2f4b4d2
Jenn Le <jenn.thakugan@icloud.com>
2017-09-18 13:57:20 -0500

    Trying to add react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from 'App';
 import registerServiceWorker from 'registerServiceWorker';
 
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+// Containers
+import App from 'App';
+import NavbarInstance from 'Nav';
+
+ReactDOM.render(<NavbarInstance />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
