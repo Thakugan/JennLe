36a0207beed2aa334469b15542236c62e864fc1a
Jenn Le <jenn.thakugan@icloud.com>
2018-01-18 22:20:00 -0600

    Fixed a nesting

--- src/components/FrameButton.js
+++ src/components/FrameButton.js
@@ -5,7 +5,7 @@ import '../styles/FrameButton.css';
 class FrameButton extends Component {
   render() {
     return(
-      <a className="frame-btn">
+      <div className="frame-btn">
         <span className="frame-btn__outline frame-btn__outline--tall">
             <span className="frame-btn__line frame-btn__line--tall"></span>
             <span className="frame-btn__line frame-btn__line--flat"></span>
@@ -16,7 +16,7 @@ class FrameButton extends Component {
         </span>
         <span className="frame-btn__solid"></span>
         <span className="frame-btn__text">{this.props.children}</span>
-      </a>
+      </div>
     );
   }
 }
