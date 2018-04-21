9ac5b4963737274da57fb1bf401db573ec7a6163
Jenn Le <jenn.thakugan@icloud.com>
2018-04-21 00:13:56 -0500

    testing webpack config

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  config.plugin('webpack-define', webpack.DefinePlugin, [
+    { 'global.GENTLY': false },
+  ])
+
+  return config
+}
