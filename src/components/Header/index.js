3499e438f3ee6a8d368ed2e097947d21e890e78b
Jenn Le <jenn.thakugan@icloud.com>
2018-04-21 00:03:24 -0500

    put outside clicks back

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,35 +4,35 @@ import Link from 'gatsby-link'
 export default class Header extends Component {
   constructor(props) {
     super(props)
-    // this.openMenu = this.openMenu.bind(this)
+    this.openMenu = this.openMenu.bind(this)
     this.state = {
       menuOpen: false,
     }
-    // this.setWrapperRef = this.setWrapperRef.bind(this)
-    // this.handleClickOutside = this.handleClickOutside.bind(this)
+    this.setWrapperRef = this.setWrapperRef.bind(this)
+    this.handleClickOutside = this.handleClickOutside.bind(this)
   }
 
-  // componentDidMount() {
-  //   document.addEventListener('mousedown', this.handleClickOutside)
-  // }
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside)
+  }
 
-  // componentWillUnmount() {
-  //   document.removeEventListener('mousedown', this.handleClickOutside)
-  // }
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside)
+  }
 
-  // setWrapperRef(node) {
-  //   this.wrapperRef = node
-  // }
+  setWrapperRef(node) {
+    this.wrapperRef = node
+  }
 
-  // handleClickOutside(event) {
-  //   if (
-  //     this.wrapperRef &&
-  //     !this.wrapperRef.contains(event.target) &&
-  //     this.state.menuOpen
-  //   ) {
-  //     this.openMenu()
-  //   }
-  // }
+  handleClickOutside(event) {
+    if (
+      this.wrapperRef &&
+      !this.wrapperRef.contains(event.target) &&
+      this.state.menuOpen
+    ) {
+      this.openMenu()
+    }
+  }
 
   openMenu() {
     const currentState = this.state.menuOpen
@@ -43,7 +43,7 @@ export default class Header extends Component {
     return (
       <div
         className={this.state.menuOpen ? 'header open-menu' : 'header'}
-        // ref={this.setWrapperRef}
+        ref={this.setWrapperRef}
       >
         <div className="brand">
           <h1 className="menu-button" onClick={this.openMenu}>
