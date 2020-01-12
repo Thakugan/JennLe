c8114b74450f77ba05c7bf39b9b6ee3ce3cc1385
thakugan <jenn.thakugan@icloud.com>
2020-01-12 15:17:56 -0800

    Stripped most of website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,70 +1,25 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react"
+import { Link } from "gatsby"
 
-import landingMobile from '../assets/landing-mobile.jpg'
-import landingDesktop from '../assets/landing-desktop.jpg'
+import SEO from "../components/seo"
 
-import { Heart, Club, Diamond } from '../assets/icons'
-import { Envelope, Github, LinkedIn } from '../assets/socialIcons'
+import "../css/index.css"
+import Layout from "../components/layout"
 
 const IndexPage = () => (
-  <div style={{ minHeight: '100%' }} className="container">
-    <div className="page landing">
-      <h1>Jenn Le</h1>
-      <h2>Aspiring Software Engineer</h2>
-      <p>
-        Hi, I’m a computer science student at Southern Methodist University. I
-        like to program and draw.
-      </p>
-      <div className="my-pic">
-        <hr />
-        <img className="mobile" src={landingMobile} />
-        <img className="desktop" src={landingDesktop} />
-        <hr />
-      </div>
-      <div className="link-content">
-        <p>
-          To learn more about me and what I can do, take a look at my{' '}
-          <Link to="/about">resume</Link> or some of the things I’ve done:
-        </p>
-        <div className="page-links">
-          <div>
-            <Link to="/projects">
-              <Heart />
-              <p>Programming Projects</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/research">
-              <Club />
-              <p>Research</p>
-            </Link>
-          </div>
-          <div>
-            <Link to="/art">
-              <Diamond />
-              <p>Art & Design</p>
-            </Link>
-          </div>
+  <Layout>
+    <div className="home">
+      <SEO title="Home" />
+      <div className='about'>
+        <img className="my-pic" src="src/images/me.jpeg" />
+        <div>
+          <h1>Jenn Le</h1>
+          <h2>Software Engineer at Microsoft</h2>
         </div>
       </div>
+      <Link to="/page-2/">Go to page 2</Link>
     </div>
-
-    <div className="filler" />
-
-    <footer className="contact">
-      <h4>Contact Me</h4>
-      <a href="mailto:jenn.thakugan@icloud.com">
-        <Envelope className="social-icon" />
-      </a>
-      <a href="https://github.com/Thakugan" target="_blank">
-        <Github className="social-icon" />
-      </a>
-      <a href="https://www.linkedin.com/in/thakugan/" target="_blank">
-        <LinkedIn className="social-icon" />
-      </a>
-    </footer>
-  </div>
+  </Layout>
 )
 
 export default IndexPage
